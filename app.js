0d8ce0c1aeaf585381d9b3ff647753e64a5deda4
DiegoVaezman <diego@netspotofficial.com>
2022-03-06 21:03:52 +0100

    cors

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.urlencoded());
       app.use(express.json());
 
       app.listen(8080, () =>
         console.log(`Server running on port: 8080`)
+      );
+      app.use(
+        cors({
+          origin: "*",
+        })
       );
       app.all("/*", function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
